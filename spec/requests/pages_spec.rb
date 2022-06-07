4b7eda295ea04bc8f1126767c345c610b1a0c76b
blessedjasonmwanza <mwanzabj@gmail.com>
2022-06-08 00:25:04 +0200

    Add test cases for the hello controller and route

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -2,6 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'Pages', type: :request do
   describe 'GET /' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'returns http success' do
+      get '/'
+      expect(response).to have_http_status(:success)
+    end
+    it 'renders the hello template' do
+      get '/'
+      expect(response).to render_template('hello')
+    end
+    it 'renders the hello text' do
+      get '/'
+      expect(response.body).to include('hello')
+    end
   end
 end
