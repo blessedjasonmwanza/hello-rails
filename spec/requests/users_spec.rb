fc84724fb2f91b3cbad77839333a9c89f907403c
blessedjasonmwanza <mwanzabj@gmail.com>
2022-06-08 00:12:46 +0200

    Test template rendering for users route

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe "Users", type: :request do
       get "/users"
       expect(response).to have_http_status(:success)
     end
+    it "renders the index template" do
+      get "/users"
+      expect(response).to render_template("index")
+    end
     it "renders Users text" do
       get "/users"
       expect(response.body).to include("Users")
