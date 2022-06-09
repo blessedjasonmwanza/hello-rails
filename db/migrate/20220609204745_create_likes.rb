0a1ebf322892c9c1d17e1bcf344aa59866fd28d1
blessedjasonmwanza <mwanzabj@gmail.com>
2022-06-09 23:08:31 +0200

    Fix relations issue and run Migrations

--- db/migrate/20220609204745_create_likes.rb
+++ db/migrate/20220609204745_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.references :author_id, null: false, foreign_key: true
-      t.references :post_id, null: false, foreign_key: true
+      t.integer :author_id, foreign_key: true
+      t.integer :post_id, foreign_key: true
 
       t.timestamps
     end
