928842caefea94a8ec296965252c51ea4137ced6
blessedjasonmwanza <mwanzabj@gmail.com>
2022-06-13 13:26:52 +0200

    Add data processing models and update table references aliases

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ApplicationRecord
-  belongs_to :author_id
+  belongs_to :author, class_name: 'User'
+
+  def increase_posts_counter
+    author.increment!(:posts_counter)
+  end
+
+  def recent_comments
+    comments.limit(5).order(created_at: :desc)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  def recent_posts
+    posts.limit(3).order(created_at: :desc)
+  end
 end
