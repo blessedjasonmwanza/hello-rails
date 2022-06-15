f9759853f86bc9d76d06c348c282fc67feeb50c3
blessedjasonmwanza <mwanzabj@gmail.com>
2022-06-15 03:34:18 +0200

    Add user_card partial and render users cards

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   # GET /users
   # GET /users.json
-  def index; end
+  def index
+    @users = User.order(:name)
+  end
 
   # GET /users/1
   # GET /users/1.json
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   has_many :likes
-
+  
   def recent_posts
     posts.limit(3).order(created_at: :desc)
   end
