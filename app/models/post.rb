465678af8e28b2e526354aee3b702461f38dcfe5
blessedjasonmwanza <mwanzabj@gmail.com>
2022-06-15 19:46:18 +0200

    render and style users & user pages

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :likes
 
+  after_save :increase_posts_counter
   def increase_posts_counter
     author.increment!(:posts_counter)
   end
