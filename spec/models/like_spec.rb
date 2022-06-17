451404910c86d932cec13e935631e4183e77cbd9
blessedjasonmwanza <mwanzabj@gmail.com>
2022-06-17 13:10:37 +0200

    Add specs for like model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  user = User.new(Name: 'Blesssed Jason Mwanza', Photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                  Bio: 'Passion Driven software developer with a keen enthusiam for creating impact driven software')
+  post = Post.create(author: user, Title: '1 post', Text: 'first post')
+  it 'tests if the Like model is created correctly' do
+    like = Like.create(author: user, post: post)
+    expect(like.author).to eq user
+    expect(like.post).to eq post
+  end
+
+  it 'updates a post\'s likes correctly' do
+    Like.create(author: user, post: post)
+    Like.create(author: user, post: post)
+    Like.create(author: user, post: post)
+    expect(post.Likes_Counter).to eq 4
+  end
 end
