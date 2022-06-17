1aca294374e3160deef39ee28db4238d37dd3fc9
blessedjasonmwanza <mwanzabj@gmail.com>
2022-06-17 13:34:01 +0200

    Add specs for post model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   it 'adds comments' do
     author = User.new(name: 'Tom', photo: 'profile_pic.jpeg', bio: 'Bio text template', posts_Counter: 0)
-    post = Post.new(title: 'New post', text: 'Good evening', author: author, likes_Counter: 0, comments_counter: 0)
+    post = Post.new(title: 'New test post', text: 'How are ', author: author, likes_Counter: 0, comments_counter: 0)
     post.save!
 
     comment_creator = User.new(name: 'Jerry', photo: 'profile_pic.jpeg', bio: 'Bio template', posts_counter: 0)
