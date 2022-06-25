d324eec90d26b52d71f94d524575d5369502a58e
Saadat Ali <saadatlucky313@gmail.com>
2022-06-25 05:53:51 +0500

    Add destroy function for comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,23 @@
 class CommentsController < ApplicationController
+  load_and_authorize_resource
   def create
     @comment = Comment.new(comment_params)
     redirect_to user_post_path(id: @comment.post_id, user_id: @comment.author_id) if @comment.save
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    authorize! :destroy, @comment
+
+    @post = Post.find(@comment.post_id)
+    
+    flash[:success] = ['Comment Deleted Successfully']
+    @comment.destroy
+
+    respond_to do |format|
+      format.html { redirect_to "/users/#{current_user.id}/posts/#{@post.id}" }
+      format.json { head :no_content }
+    end
   end
 
   private
